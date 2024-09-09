4d0c57ddae36be653700bf8a74ee12b45ddc84eb
Mike Griniezakis <griniezakismike12@gmail.com>
2024-09-09 19:31:36 +0300

    update config

--- init.lua
+++ init.lua
@@ -871,12 +871,32 @@ require('lazy').setup({
     branch = 'regexp', -- This is the regexp branch, use this for the new version
     config = function()
       require('venv-selector').setup()
+      require('dap-python').setup 'python'
     end,
     keys = {
       { ',v', '<cmd>VenvSelect<cr>' },
     },
   },
-  { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' } },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    config = function()
+      require('dapui').setup()
+      local dap, dapui = require 'dap', require 'dapui'
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
   {
     'folke/trouble.nvim',
     opts = {}, -- for default options, refer to the configuration section for custom setup.
